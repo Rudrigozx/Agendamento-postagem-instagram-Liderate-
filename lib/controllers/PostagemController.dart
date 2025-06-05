5eec5cf31eebf92f9f4a36a81768e13c8ba06b74
rudrigozx <rodrigo.leandro.zx@gmail.com>
2025-06-05 00:09:08 -0300

    adicionando controller com funçoes de criar e listar com persistencia de dados

---
+++ lib/controllers/PostagemController.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:agendamento_postagens/models/Postagem.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class PostagemController {
+  static const String _key = 'postagens';
+
+  Future<void> criar(String titulo, String descricao, DateTime dataAgendamento, TimeOfDay horaAgendamento) async {
+    final prefs = await SharedPreferences.getInstance();
+    final postagem = Postagem(
+      titulo: titulo,
+      legenda: descricao,
+      dataAgendamento: dataAgendamento,
+      horaAgendamento: horaAgendamento,
+    );
+
+    List<String> listaSalva = prefs.getStringList(_key) ?? [];
+    listaSalva.add(jsonEncode(postagem.toJson()));
+    await prefs.setStringList(_key, listaSalva);
+  }
+
+  Future<List<Postagem>> listar() async {
+    final prefs = await SharedPreferences.getInstance();
+    final lista = prefs.getStringList(_key) ?? [];
+
+    return lista
+        .map((item) => Postagem.fromJson(jsonDecode(item)))
+        .toList();
+  }
+}
